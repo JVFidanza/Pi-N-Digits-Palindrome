4f4fec61ad58d87e5f5d4b92345c04948e35656a
joao.artidao.ext <joao.artidao.ext@brf.com>
2022-11-11 11:09:15 -0300

    fix: renaming files and namespaces

--- PalindromePiPrimeNDigits/Program.cs
+++ PalindromePiPrimeNDigits/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Threading;
 
-namespace PalindromePrimePi21
+namespace PalindromePrimePiNDigits
 {
     internal class Program
     {
--- PalindromePiPrimeNDigits/Properties/AssemblyInfo.cs
+++ PalindromePiPrimeNDigits/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PalindromePi9")]
+[assembly: AssemblyTitle("PalindromePiPrimeNDigits")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PalindromePi9")]
+[assembly: AssemblyProduct("PalindromePiPrimeNDigits")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
